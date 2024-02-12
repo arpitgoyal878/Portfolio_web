afe239508f16076b0ce760838ce880c42af1502f
Arpit <arpitgoyal878@gmail.com>
2024-02-13 01:43:56 +0530

    removed ga and tried to use posthog for analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useEffect } from "react";
+import { lazy, Suspense } from "react";
 
 const Header = lazy(() => import("./components/Header"));
 const Hero = lazy(() => import("./components/Hero"));
@@ -6,16 +6,9 @@ const Project = lazy(() => import("./components/Project"));
 const Skill = lazy(() => import("./components/Skill"));
 const Contact = lazy(() => import("./components/Contact"));
 
-import ReactGA from "react-ga";
-//tracting ID
-
-const TrackingId = "UA-302303477-1";
-ReactGA.initialize(TrackingId);
-
 function App() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search)
-  }, []);
+
+  
   return (
     <Suspense
       fallback={
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { PostHogProvider} from 'posthog-js/react'
+//todo:make sure to replace the PUBLIC_POSTHOG_HOST and PUBLIC_POSTHOG_KEY as a env variables later on
+const PUBLIC_POSTHOG_HOST ='phc_sBCVtkw87IViWsfYORaj2cKf6Tq1ps7eUxrpPXoYZJJ';
+const PUBLIC_POSTHOG_KEY = 'https://app.posthog.com';
+
+
+const options = {
+  api_host: PUBLIC_POSTHOG_HOST,
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <PostHogProvider 
+      apiKey={PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
     <App />
+    </PostHogProvider>
   </React.StrictMode>,
 )
