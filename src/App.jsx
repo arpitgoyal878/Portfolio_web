77fb6b50980092ab239ae8887b48282e65dcb301
Arpit <arpitgoyal878@gmail.com>
2024-02-13 00:54:24 +0530

    added some google analytics features to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useEffect } from "react";
 
-const Header = lazy(() => import('./components/Header'));
-const Hero = lazy(() => import('./components/Hero'));
-const Project = lazy(() => import('./components/Project'));
-const Skill = lazy(() => import('./components/Skill'));
-const Contact = lazy(() => import('./components/Contact'));
+const Header = lazy(() => import("./components/Header"));
+const Hero = lazy(() => import("./components/Hero"));
+const Project = lazy(() => import("./components/Project"));
+const Skill = lazy(() => import("./components/Skill"));
+const Contact = lazy(() => import("./components/Contact"));
+
+import ReactGA from "react-ga";
+//tracting ID
+
+const TrackingId = "UA-302303477-1";
+ReactGA.initialize(TrackingId);
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, []);
   return (
     <Suspense
       fallback={
-        <div className='sk-folding-cube'>
-          <div className='sk-cube1 sk-cube'></div>
-          <div className='sk-cube2 sk-cube'></div>
-          <div className='sk-cube4 sk-cube'></div>
-          <div className='sk-cube3 sk-cube'></div>
+        <div className="sk-folding-cube">
+          <div className="sk-cube1 sk-cube"></div>
+          <div className="sk-cube2 sk-cube"></div>
+          <div className="sk-cube4 sk-cube"></div>
+          <div className="sk-cube3 sk-cube"></div>
         </div>
       }
     >
-      <div className='App container my-10 mx-auto max-w-screen-lg bg-black'>
+      <div className="App container my-10 mx-auto max-w-screen-lg bg-black">
         <Header />
         <main>
           <Hero />
